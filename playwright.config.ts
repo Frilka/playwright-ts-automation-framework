af8313eca0a2b3a3a700077dfb4dc3cefdf4bc0d
Yuliia <yuliia@Yuliias-Laptop.local>
2026-04-03 00:46:09 +0300

    Fix Browsers settings

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,10 +23,10 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
     // {
     //   name: 'webkit',
