4a67127cd61b601558a545d6b4f9dd5ded3bf675
Froll <frolovajul@gmail.com>
2026-04-03 00:53:33 +0300

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   retries: 0,
   use: {
     baseURL: ConfigManager.getBaseUrl(),
-    headless: false,
+    headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
   },
